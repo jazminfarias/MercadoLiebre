497879a318df697859c923e16cdce15d8cf8daf0
jazminfarias <jazminfarias07@gmail.com>
2022-12-14 20:43:35 -0300

    correccion app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require ('path');
 
 const port= process.env.PORT || 3000;
-app.listen(port,()=>console.log('Servidor corriendo en el puerto' + PORT));
+app.listen(port,()=>console.log('Servidor corriendo en el puerto ' + port));
 
 app.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));});
